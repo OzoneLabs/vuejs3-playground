6cd402843a0d586065cd57793ecaa383fcb0bc0c
Trirst <abeces968@gmail.com>
2022-12-14 23:07:38 +0700

    WIP login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -29,43 +29,40 @@ export default {
     <el-card class="box">
         <el-tabs v-model="activeTab">
             <el-tab-pane label="Login" name="login">
-        <form>
-            <div class="mg-sm">
-                <el-input v-model="username" placeholder="Username" />
-            </div>
+                <form>
+                    <div class="mg-sm">
+                        <el-input v-model="username" placeholder="Username" />
+                    </div>
 
-            <div class="mg-sm">
-                <el-input type="password" v-model="password" placeholder="Password" />
-            </div>
+                    <div class="mg-sm">
+                        <el-input type="password" v-model="password" placeholder="Password" />
+                    </div>
 
-            <el-row justify="space-between">
-                <el-col :span="10">
-                    <el-checkbox label="Remember the password" />
-                </el-col>
-                <el-col :span="6">
-                    <el-link type="primary">Forgot password</el-link>
-                </el-col>
-            </el-row>
+                    <el-row justify="space-between">
+                        <el-col :span="10">
+                            <el-checkbox label="Remember the password" />
+                        </el-col>
+                        <el-col :span="6">
+                            <el-link type="primary">Forgot password</el-link>
+                        </el-col>
+                    </el-row>
 
-            <div class="text-center">
-                <el-button @click="login" type="primary" round class="login-button">Login</el-button>
-            </div>
-        </form>
+                    <div class="text-center">
+                        <el-button @click="login" type="primary" round class="login-button">Login</el-button>
+                    </div>
+                </form>
             </el-tab-pane>
 
             <el-tab-pane label="Register" name="register">
                 <h1>TODO</h1>
             </el-tab-pane>
         </el-tabs>
-
-
     </el-card>
 </template>
 
 <style scoped>
 .box {
-    width: 560px;
-    /* height: 300px; */
+    width: 440px;
     /* padding: 20px; */
 }
 
@@ -78,6 +75,8 @@ export default {
 }
 
 .login-button {
-    width: 480px;
+    /* padding: 20em; */
+    width: 25em;
+    margin: 20px 0;
 }
 </style>
