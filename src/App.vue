d540d82b1d7d2cd41340a8aae3c6b1df14994ec1
Trirst <abeces968@gmail.com>
2022-12-15 23:28:44 +0700

    Crude login-register page

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
 <script>
-import LoginRegisterCard from './components/LoginRegisterCard.vue';
+import LoginRegisterPage from './views/LoginRegisterPage.vue';
 
 export default {
     components: {
-        LoginRegisterCard: LoginRegisterCard,
+        LoginRegisterPage,
     },
 }
 </script>
 
 <template>
-    <div id="background" class="bg-strong-blue full-screen left-top-padding">
-        <LoginRegisterCard />
-    </div>
+    <LoginRegisterPage />
 </template>
-
-<style scoped>
-    .bg-strong-blue {
-        background-color: #293246;
-    }
-
-    .full-screen {
-        width: 100vw;
-        height: 100vh;
-    }
-
-    .left-top-padding {
-        padding-top: 10px;
-        padding-left: 10px;
-    }
-</style>
---
+++ src/views/LoginRegisterPage.vue
@@ -0,0 +1,64 @@
+<script>
+import LoginRegisterCard from '../components/LoginRegisterCard.vue';
+
+export default {
+    components: {
+        LoginRegisterCard: LoginRegisterCard,
+    },
+}
+</script>
+
+<template>
+    <el-row class="strong-blue-bg full-screen">
+        <el-col :span="12" class="white-text center-flex col-flex">
+            <div style="width: 520px;">
+                <img src="../assets/loginregister-illustration.svg" width="320" />
+                <div class="big-text">Out of the box in the background management system</div>
+                <div class="small-text">Click login for a quick experience</div>
+            </div>
+        </el-col>
+
+        <el-col :span="12" class="center-flex">
+            <LoginRegisterCard />
+        </el-col>
+    </el-row>
+</template>
+
+<style scoped>
+.strong-blue-bg {
+    background-color: #293246;
+}
+
+.big-text {
+    font-weight: bold;
+    font-size: 30px;
+}
+
+.small-text {
+    margin-top: 20px;
+}
+
+.white-text {
+    color: #fff;
+}
+
+.center-flex {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.col-flex {
+    flex-direction: column;
+}
+
+.full-screen {
+    width: 100vw;
+    height: 100vh;
+}
+
+.left-top-padding {
+    padding-top: 10px;
+    padding-left: 10px;
+}
+</style>
