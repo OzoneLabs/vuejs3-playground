51736f983340deecee6643df2c8b72e97ce9bc65
Trirst <abeces968@gmail.com>
2022-12-16 22:05:27 +0700

    Install iconify for vue

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'element-plus/dist/index.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import './assets/main.css'
 import 'virtual:windi.css'
+import { Icon } from '@iconify/vue';
 
 const app = createApp(App);
 
@@ -12,6 +13,9 @@ const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
+
+// Register iconify icons
+app.component('Icon', Icon);
 
 app.use(ElementPlus);
 app.mount('#app');
