555b730318f774c605f4e74deff585bbb735a878
Trirst <abeces968@gmail.com>
2022-12-16 23:29:03 +0700

    Add header to login-register page

--- src/views/LoginRegisterPage.vue
+++ src/views/LoginRegisterPage.vue
@@ -9,19 +9,36 @@ export default {
 </script>
 
 <template>
-    <el-row class="bg-[#293246] w-screen h-screen">
-        <el-col :span="12" class="text-white flex-center flex-col">
-            <div style="width: 520px;">
-                <img src="../assets/loginregister-illustration.svg" width="320" />
-                <div class="font-semibold text-30px">Out of the box in the background management system</div>
-                <div class="mt-20px">Click login for a quick experience</div>
-            </div>
-        </el-col>
+    <el-container class="bg-[#293246] w-screen h-screen">
+        <el-header class="pt-4 px-8">
+            <el-row justify="space-between">
+                <div class="text-white text-xl">
+                    <Icon icon="logos:vue" class="inline-block mr-3" />
+                    <span>vue3-admin</span>
+                </div>
 
-        <el-col :span="12" class="flex-center">
-            <LoginRegisterCard />
-        </el-col>
-    </el-row>
+                <button>
+                    <Icon icon="ph:translate-bold" color="white" />
+                </button>
+            </el-row>
+        </el-header>
+
+        <el-main>
+            <el-row class="w-full h-full">
+                <el-col :span="12" class="text-white flex-center flex-col">
+                    <div class="max-w-520px">
+                        <img src="../assets/loginregister-illustration.svg" width="320" />
+                        <div class="font-semibold text-30px">Out of the box in the background management system</div>
+                        <div class="mt-20px">Click login for a quick experience</div>
+                    </div>
+                </el-col>
+
+                <el-col :span="12" class="flex-center">
+                    <LoginRegisterCard />
+                </el-col>
+            </el-row>
+        </el-main>
+    </el-container>
 </template>
 
 <style scoped>
